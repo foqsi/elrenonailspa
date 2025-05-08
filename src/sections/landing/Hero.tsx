78d2830d9a83a3c870c49eb7a751f99ffa869c74
foqsi <foqsi03@gmail.com>
2025-05-08 10:59:15 -0500

    modified:   src/sections/landing/Hero.tsx

--- src/sections/landing/Hero.tsx
+++ src/sections/landing/Hero.tsx
@@ -3,7 +3,7 @@ export default function Hero() {
     <section className="bg-red-600 text-white py-20">
       <div className="container mx-auto px-4 text-center">
         <h1 className="text-4xl md:text-5xl font-bold">Welcome to El Reno Nail Spa</h1>
-        <p className="mt-4 text-lg md:text-xl">"Nail Perfection, Every Time" 💅</p>
+        <p className="mt-4 text-lg md:text-xl">&quot;Nail Perfection, Every Time&quot; 💅</p>
       </div>
 
     </section>
