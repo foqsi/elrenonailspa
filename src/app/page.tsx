f01bf1c51392892091fcf7c0def9969169313910
foqsi <foqsi03@gmail.com>
2025-05-08 18:36:35 -0500

    added metadata and preview image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-'use client';
+// app/page.tsx
 
+import { Metadata } from 'next';
 import Landing from '@/sections/landing/Landing';
 
+export const metadata: Metadata = {
+  title: 'El Reno Nail Spa',
+  description: 'Nail Perfection, Every Time! Walk-ins & Appointments are welcome!',
+  openGraph: {
+    title: 'El Reno Nail Spa',
+    description: 'Nail Perfection, Every Time! Walk-ins & Appointments are welcome!',
+    url: 'https://elrenonailspa.com',
+    images: [
+      {
+        url: 'https://elrenonailspa.com/images/preview.png',
+        width: 1200,
+        height: 630,
+        alt: 'El Reno Nail Spa Preview',
+      },
+    ],
+  },
+};
+
 export default function HomePage() {
-  return <Landing />;
+  return (
+    <Landing />
+  );
 }
